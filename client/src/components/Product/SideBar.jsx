bc851d6d566ad2ad8561a83cb0a4214390bbca38
PrathameshPawar119 <cm.b26.prathameshpawar@gmail.com>
2023-11-29 13:31:19 +0530

    initial

--- client/src/components/Product/SideBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SideBar = () => {
-    return <div className="w-1/5 h-[400px] rounded-md bg-gray-100 mx-2">
-        <div>Filter Component</div>
-    </div>;
-};
-
-export default SideBar;
--- client/src/pages/Community.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Community() {
-  return (
-    <div>
-          <h1>Community</h1>
-    </div>
-  )
-}
